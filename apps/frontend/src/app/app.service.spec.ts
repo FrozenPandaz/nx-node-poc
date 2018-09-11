d08f7aa42d69a3abf846c126c39de9385fa00205
Jason Jean <jasonjean1993@gmail.com>
2018-09-11 15:07:26 -0400

    progress

--- apps/frontend/src/app/app.service.spec.ts
+++ apps/frontend/src/app/app.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { AppService } from './app.service';
 
 describe('AppService', () => {
+  let service: AppService;
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -11,10 +16,29 @@ describe('AppService', () => {
     });
   });
 
-  it(
-    'should be created',
-    inject([AppService], (service: AppService) => {
-      expect(service).toBeTruthy();
-    })
+  beforeEach(
+    inject(
+      [AppService, HttpTestingController],
+      (_service: AppService, _controller) => {
+        service = _service;
+        httpTestingController = _controller;
+      }
+    )
   );
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getData', () => {
+    it('should retrieve data', () => {
+      const testData = { data: 'data' };
+      service.getData().subscribe(data => {
+        expect(data).toEqual(testData);
+      });
+      const request = httpTestingController.expectOne('/data');
+      request.flush(testData);
+      httpTestingController.verify();
+    });
+  });
 });
