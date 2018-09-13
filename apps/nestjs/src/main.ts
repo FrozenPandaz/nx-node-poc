1e079bee2d4c8fc34d7fcc201d4bd2b83e6547e4
Jason Jean <jasonjean1993@gmail.com>
2018-09-13 15:04:56 -0400

    progress

--- apps/nestjs/src/main.ts
+++ apps/nestjs/src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, FastifyAdapter } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 console.log(environment);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter(), {
     logger: false
   });
   await app.listen(3000);
